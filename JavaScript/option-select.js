a6b19a29f2b0d8eb1fd21f1c8917ee3b9d880975
Noel John Robert <noel060202@gmail.com>
2023-05-15 10:46:45 +0530

    error correction and code formatting

--- JavaScript/option-select.js
+++ JavaScript/option-select.js
@@ -1,37 +1,32 @@
 function updateListbox(category) {
-    var options = [
-    {value: "option1", text: "journal1"},
-    {value: "option2", text: "journal2"},
-    {value: "option3", text: "journal3"},
-    {value: "option4", text: "journal4"}
-    ];
-    
-var selectBox = document.getElementById(category);
-selectBox.innerHTML = "";
-var filteredOptions = options.filter(function(option) {
-return option.value.startsWith(category);
-});
-filteredOptions.forEach(function(option) {
-var newOption = document.createElement("option");
-newOption.value = option.value;
-newOption.text = option.text;
-selectBox.add(newOption);
-});
-}
-function setList()
-{
-var type = document.getElementById("article-type");
-for (var i = 0; i < articlename.options.length; i++)
-{
-var op=document.getElementById("op");
-if(type=='Journal')
-{
-updateListbox(o1);
-updateListbox(o2);
-updateListbox(o3);
-updateListbox(o4);
-}
+  var options = [
+    { value: "option1", text: "journal1" },
+    { value: "option2", text: "journal2" },
+    { value: "option3", text: "journal3" },
+    { value: "option4", text: "journal4" },
+  ];
 
+  var selectBox = document.getElementById(category);
+  selectBox.innerHTML = "";
+  var filteredOptions = options.filter(function (option) {
+    return option.value.startsWith(category);
+  });
+  filteredOptions.forEach(function (option) {
+    var newOption = document.createElement("option");
+    newOption.value = option.value;
+    newOption.text = option.text;
+    selectBox.add(newOption);
+  });
 }
-
+function setList() {
+  var type = document.getElementById("article-type");
+  for (var i = 0; i < articlename.options.length; i++) {
+    var op = document.getElementById("op");
+    if (type == "Journal") {
+      updateListbox(o1);
+      updateListbox(o2);
+      updateListbox(o3);
+      updateListbox(o4);
+    }
+  }
 }
