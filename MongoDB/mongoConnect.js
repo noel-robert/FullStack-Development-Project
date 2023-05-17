7f0883afefb0e8b238461b770027ab96b23d6140
Noel John Robert <noel060202@gmail.com>
2023-05-17 19:29:27 +0530

    Added boilerplate programs to build on

---
+++ MongoDB/mongoConnect.js
@@ -0,0 +1,16 @@
+const { MongoClient } = require('mongodb');
+
+// connection urls
+const url = 'mongodb://127.0.0.1:27017';
+const client = new MongoClient(url);
+
+const userDB = '';
+const authorDB = '';
+const articleDB = '';
+
+async function main() {}
+
+main()
+    .then(console.log)
+    .catch(console.error)
+    .finally(() => client.close())
