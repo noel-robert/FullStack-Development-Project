47a09d5294e565876cd420ab06060834ddafa58d
Noel John Robert <noel060202@gmail.com>
2023-06-03 00:58:45 +0530

    search_value field kept disabled until needed

---
+++ JavaScript/javascriptFunctions.js
@@ -0,0 +1,11 @@
+// search_value textfield kept disabled untill needed
+function changeAccess() {
+    var article_type = document.getElementById("article_type");
+    var search_field = document.getElementById("search_field");
+    
+
+    if (article_type.value!="-" && search_field.value!="-")
+        document.getElementById("search_value").disabled = false;
+    else
+        document.getElementById("search_value").disabled = true;
+}
